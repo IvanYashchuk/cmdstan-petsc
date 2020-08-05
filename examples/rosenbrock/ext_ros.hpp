eed24a8054e556ac01d3c66e47b114d6bfb6528c
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-08-05 18:50:17 +0300

    Modify ext_ros

--- examples/rosenbrock/ext_ros.hpp
+++ examples/rosenbrock/ext_ros.hpp
@@ -1,19 +1,19 @@
-#include <stan/math.hpp>
-#include <boost/math/tools/promotion.hpp>
+// #include <stan/math.hpp>
+// #include <boost/math/tools/promotion.hpp>
 
-using namespace stan::math;
+// using namespace stan::math;
 
 namespace rosenbrock_model_namespace {
 
 template <typename T0__>
-Eigen::Matrix<typename boost::math::tools::promote_args<T0__>::type, -1, 1>
-my_rosenbrock(const Eigen::Matrix<T0__, -1, 1>& xy, std::ostream* pstream__) 
+Eigen::Matrix<stan::promote_args_t<T0__>, -1, 1>
+my_rosenbrock(const Eigen::Matrix<T0__, -1, 1>& xy, std::ostream* pstream__)
 {
     // throw std::logic_error("not implemented");  // this should never be called
     // typedef typename boost::math::tools::promote_args<T0__>::type T;
-    if (xy.size() > 2) {
-        throw std::logic_error("This function is implemented only for input of size 2.");
-    }
+    // if (xy.size() > 2) {
+    //     throw std::logic_error("This function is implemented only for input of size 2.");
+    // }
     using T = stan::return_type_t<T0__>;
     T x = xy(0);
     T y = xy(1);
